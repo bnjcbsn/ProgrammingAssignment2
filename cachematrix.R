3a7ba4f7515607f2ec470b38eada32347665bf64
bnjcbsn <bencrusty@gmail.com>
2015-01-25 16:48:50 -0600

    updated with new code
    modified to calculate for matrix caching functionality

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,29 @@
 ## functions do
 
 ## Write a short comment describing this function
+## create a wrapping function so that results are cached
+## when function(s) in the list are called, they operate
+## and exist with a static environment
+## much credit to
+## https://class.coursera.org/rprog-010/forum/thread?thread_id=364#post-2000
+
+
+## new_matrix_env<- makeCacheMatrix(matrix)
+## cacheSolve(new_matrix_env)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- matinv
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
@@ -12,4 +32,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
